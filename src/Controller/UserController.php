0b98a0ae263e6f9101f34a1b1e2d7c077e65c5b4
jberaldo <joaoangelio409@gmail.com>
2024-01-29 21:19:10 -0300

    Logging

---
+++ src/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+// src/Controller/UserController.php
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+#[Route('/user', name: 'user_')]
+class UserController extends AbstractController
+{
+    public function __construct(
+        private readonly UserRepository $userRepository
+    )
+    {}
+
+    #[Route('/', name: 'create_', methods: ['POST'])]
+    public function store(Request $request): JsonResponse
+    {
+        return $this->json([
+            "user" => $this->userRepository->store($request)
+        ]);
+    }
+}
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+/**
+ * @extends ServiceEntityRepository<User>
+ *
+ * @implements PasswordUpgraderInterface<User>
+ *
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct(
+        ManagerRegistry $registry,
+        private readonly LoggerInterface $logger
+    )
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
+
+    public function store()
+    {
+        $this->logger->info('Abacaxi');
+        $user = new User();
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
+}
